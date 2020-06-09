9c5146749e41418f76a40a8e39035dbe633ac735
Tyler Trudgeon <tyler.r.trudgeon@gmail.com>
2020-06-09 14:51:01 -0500

    working on basic python programs

---
+++ basic-window.py
@@ -0,0 +1,21 @@
+import pygame
+
+def main():
+
+    pygame.init()
+
+    logo = pygame.image.load("logo32x32.png")
+    pygame.display.set_icon(logo)
+    pygame.display.set_caption("basic program")
+
+    screen = pygame.display.set_mode((240, 180))
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.quit():
+                running = False
+
+if __name__=="__main__":
+    main()
